aefd1c1557c9c6d01421a8fa1fad5bcf9fb47647
teppeis <teppeis@gmail.com>
2014-10-07 04:14:30 +0900

    use drip

--- compile.sh
+++ compile.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+JAVA=java
+type drip > /dev/null
+if [ $? = 0 ]; then
+    JAVA=drip
+fi
+
 for DIR in $(ls ./build); do
     DIR=./build/$DIR
     echo "- $DIR"
-    java -jar compiler.jar --language_in ECMASCRIPT6 --language_out ECMASCRIPT3 --formatting PRETTY_PRINT -O SIMPLE --js "$DIR/in.js" > $DIR/out.js
+    $JAVA -jar compiler.jar --language_in ECMASCRIPT6 --language_out ECMASCRIPT3 --formatting PRETTY_PRINT -O SIMPLE --js "$DIR/in.js" > $DIR/out.js
 done
