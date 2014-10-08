f371227a4e4508654ad776197719533fe7231cd3
teppeis <teppeis@gmail.com>
2014-10-08 23:37:22 +0900

    use fast-closure-compiler

--- compile.sh
+++ compile.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-JAVA=java
-type drip > /dev/null
-if [ $? = 0 ]; then
-    JAVA=drip
-fi
+JAVA=./node_modules/fast-closure-compiler/bin/closure
 
 for DIR in $(ls ./build); do
     DIR=./build/$DIR
     echo "- $DIR"
-    $JAVA -jar compiler.jar --language_in ECMASCRIPT6 --language_out ECMASCRIPT3 --formatting PRETTY_PRINT -O SIMPLE --js "$DIR/in.js" > $DIR/out.js
+    $JAVA --language_in ECMASCRIPT6 --language_out ECMASCRIPT3 --formatting PRETTY_PRINT -O SIMPLE --js "$DIR/in.js" > $DIR/out.js
 done
