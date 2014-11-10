acd31ae87c3bf021201fe82313a37bf0f3b2ab5b
teppeis <teppeis@gmail.com>
2014-11-10 21:28:08 +0900

    Add error log

--- compile.sh
+++ compile.sh
@@ -2,10 +2,27 @@
 
 JAVA=./node_modules/fast-closure-compiler/bin/closure
 
-for DIR in $(ls ./build); do
+VERSION=$($JAVA --version|grep Version|sed -e 's/Version: //g')
+echo $VERSION
+ERRORLOG=./result/$VERSION.error.txt
+rm $ERRORLOG
+
+for DIR in $(ls -I filelist.json -v ./build); do
     DIR=./build/$DIR
-    echo "- $DIR"
-    $JAVA --language_in ECMASCRIPT6 --language_out ECMASCRIPT3 --formatting PRETTY_PRINT -O SIMPLE --js "$DIR/in.js" > $DIR/out.js
+    $JAVA --language_in ECMASCRIPT6 --language_out ECMASCRIPT3 --formatting PRETTY_PRINT -O SIMPLE --js "$DIR/in.js" > $DIR/out.js 2> ./error
+    # exit code of JNI is wrong
+    if [ "$(cat ./error)" != "" ]; then
+        echo "- $DIR: NG"
+
+        cat $DIR/in.js >> $ERRORLOG
+        echo "" >> $ERRORLOG
+        cat ./error >> $ERRORLOG
+        echo "" >> $ERRORLOG
+    else
+        echo "- $DIR: OK"
+    fi
 done
+
+rm error
 
 exit 0
