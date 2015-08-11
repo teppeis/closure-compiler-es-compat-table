672d6590425ebb4c5a827ac539437d3aaade56de
teppeis <teppeis@gmail.com>
2015-08-12 00:52:37 +0900

    add __createIterableObject

--- check.js
+++ check.js
@@ -4,6 +4,24 @@ var fs = require('fs');
 var path = require('path');
 var files = require('./build/filelist.json');
 
+global.__createIterableObject = function(arr, methods) {
+  methods = methods || {};
+  if (typeof Symbol !== 'function' || !Symbol.iterator) {
+    return {};
+  }
+  arr.length++;
+  var iterator = {
+    next: function() {
+      return { value: arr.shift(), done: arr.length <= 0 };
+    },
+    'return': methods['return'],
+    'throw': methods['throw']
+  };
+  var iterable = {};
+  iterable[Symbol.iterator] = function(){ return iterator; }
+  return iterable;
+}
+
 var results = [];
 
 var fileno = 0;
