1c62f8f98b9ad6ee82ae2b74da6a44db55f5a5df
teppeis <teppeis@gmail.com>
2018-04-17 01:02:06 +0900

    fix(bulk): reorder

--- bulk.sh
+++ bulk.sh
@@ -7,14 +7,14 @@ reset () {
     git ci -m "update $1"
 }
 
-reset es6/v20180204
-reset es2016plus/v20180204
-reset esnext/v20180204
+reset es6/v20180402
+reset es2016plus/v20180402
+reset esnext/v20180402
 
 reset es6/v20180319
 reset es2016plus/v20180319
 reset esnext/v20180319
 
-reset es6/v20180402
-reset es2016plus/v20180402
-reset esnext/v20180402
+reset es6/v20180204
+reset es2016plus/v20180204
+reset esnext/v20180204
