afd1137ffe6f5d6422baa0bf66e025eef226054a
teppeis <teppeis@gmail.com>
2018-04-17 04:34:21 +0900

    fix(alter-tests): fix tests for Number.MIN/MAX_SAFE_INTEGER

--- alter-tests/es6/built-in_extensions/Number_properties/Number.MAX_SAFE_INTEGER/alter.js
+++ alter-tests/es6/built-in_extensions/Number_properties/Number.MAX_SAFE_INTEGER/alter.js
@@ -1,5 +1,6 @@
 // built-in extensions / Number properties / Number.MAX_SAFE_INTEGER
 module.exports = () => {
-  // NOTE: this code seems not to cause polyfill insertion
-  return Number.MAX_SAFE_INTEGER === Math.pow(2, 53) - 1;
+  // NOTE: this assinment is needed to insert polyfill
+  var n = Number.MAX_SAFE_INTEGER;
+  return n === Math.pow(2, 53) - 1;
 };
