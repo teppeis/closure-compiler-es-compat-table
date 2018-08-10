704b91c5da5b3720e45e2703406d7d05df7cff3f
teppeis <teppeis@gmail.com>
2018-08-10 22:26:04 +0900

    update esnext tests

--- alter-tests/esnext/candidate__stage_3_/Object.fromEntries/orig.js
+++ alter-tests/esnext/candidate__stage_3_/Object.fromEntries/orig.js
@@ -1,4 +1,4 @@
-// draft (stage 2) / Object.fromEntries
+// candidate (stage 3) / Object.fromEntries
 module.exports = () => {
   var object = Object.fromEntries(new Map([['foo', 42], ['bar', 23]]));
   return object.foo === 42 && object.bar === 23;
