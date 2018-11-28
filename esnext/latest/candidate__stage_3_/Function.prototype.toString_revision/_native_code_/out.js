4d2a2a6afae9b9d46f452c336970c5b755a4a79e
teppeis <teppeis@gmail.com>
2018-11-29 00:57:59 +0900

    copy v20181028 to latest

--- esnext/latest/candidate__stage_3_/Function.prototype.toString_revision/_native_code_/out.js
+++
@@ -1,4 +0,0 @@
-module.exports = function() {
-  return /\bfunction\b[\s\S]*\beval\b[\s\S]*\([\s\S]*\)[\s\S]*\{[\s\S]*\[[\s\S]*\bnative\b[\s\S]+\bcode\b[\s\S]*\][\s\S]*\}/.test(eval + "");
-};
-
--- esnext/latest/candidate__stage_3_/Function.prototype.toString_revision/functions_created_with_the_Function_constructor/in.js
+++
@@ -1,8 +0,0 @@
-// candidate (stage 3) / Function.prototype.toString revision / functions created with the Function constructor
-module.exports = () => {
-  throw new Error('eval() and Function() cannot be transpiled');
-  var fn = Function('a', ' /\x2A a \x2A/ b, c /\x2A b \x2A/ //', '/\x2A c \x2A/ ; /\x2A d \x2A/ //');
-  var str = 'function anonymous(a, /\x2A a \x2A/ b, c /\x2A b \x2A/ //\n) {\n/\x2A c \x2A/ ; /\x2A d \x2A/ //\n}';
-  return fn + '' === str;
-
-};
