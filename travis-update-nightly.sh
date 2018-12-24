6e06fc8479d8099c4eb51cc547b266b07ef76a81
Teppei Sato <teppeis@gmail.com>
2018-12-25 00:54:28 +0900

    ci: update nightly with travis cron (#55)

---
+++ travis-update-nightly.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eux
+
+git checkout "$TRAVIS_BRANCH"
+
+curl -L git.io/nodebrew | perl - setup
+export PATH=$HOME/.nodebrew/current/bin:$PATH
+nodebrew install 0.10
+
+version=$(npm i --no-save google-closure-compiler@nightly | grep google-closure-compiler@ | sed -e 's/.*@//')
+echo "version: $version"
+./run.js es6/1.0-SNAPSHOT
+./run.js es2016plus/1.0-SNAPSHOT
+./run.js esnext/1.0-SNAPSHOT
+
+if [ -z "$(git status --porcelain)" ]; then
+    echo "No updates"
+    exit
+fi
+
+echo "Updated"
+git diff
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git add .
+git commit --message "update nightly result with $version (travis: $TRAVIS_BUILD_NUMBER) [skip ci]"
+git push "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "HEAD:nightly-tmp"
