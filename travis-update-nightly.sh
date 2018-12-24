e3e8b9e9b532cf8b2de84b3ff9f82efb032e2634
teppeis <teppeis@gmail.com>
2018-12-24 23:23:55 +0900

    ci: install nodebrew

--- travis-update-nightly.sh
+++ travis-update-nightly.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -eu
 
+curl -L git.io/nodebrew | perl - setup
+export PATH=$HOME/.nodebrew/current/bin:$PATH
+nodebrew install 0.10
+
 version=$(npm i --no-save google-closure-compiler@nightly | grep google-closure-compiler@ | sed -e 's/.*@//')
-./run.js es6/1.0-SNAPSHOT
-./run.js es2016plus/1.0-SNAPSHOT
+# ./run.js es6/1.0-SNAPSHOT
+# ./run.js es2016plus/1.0-SNAPSHOT
 ./run.js esnext/1.0-SNAPSHOT
 
 if [ -z "$(git status --porcelain)" ]; then
