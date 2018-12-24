c52d7a12e1d9412786a843c08f68123f99b86e1b
teppeis <teppeis@gmail.com>
2018-12-24 18:40:33 +0900

    ci: update nightly with travis cron

---
+++ travis-update-nightly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+version=$(npm i --no-save google-closure-compiler@nightly | grep google-closure-compiler@ | sed -e 's/.*@//')
+./run.js es6/1.0-SNAPSHOT
+./run.js es2016plus/1.0-SNAPSHOT
+./run.js esnext/1.0-SNAPSHOT
+
+if [ -z "$(git status --porcelain)" ]; then
+    exit
+fi
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git add .
+git commit --message "update nightly result with $version (travis #$TRAVIS_BUILD_NUMBER)"
+# git push "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "$TRAVIS_BRANCH"
