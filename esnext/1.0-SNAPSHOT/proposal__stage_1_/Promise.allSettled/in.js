2dcf112a9d48795985702b30b13a5c907372c03b
teppeis <teppeis@gmail.com>
2018-12-27 22:20:46 +0900

    update nightly

---
+++ esnext/1.0-SNAPSHOT/proposal__stage_1_/Promise.allSettled/in.js
@@ -0,0 +1,16 @@
+// proposal (stage 1) / Promise.allSettled
+module.exports = (asyncTestPassed) => {
+  Promise.allSettled([
+    Promise.resolve(1),
+    Promise.reject(2),
+    Promise.resolve(3)
+  ]).then(it => {
+    if (
+      it.length === 3 &&
+it[0].status === 'fulfilled' && it[0].value === 1 &&
+it[1].status === 'rejected' && it[1].reason === 2 &&
+it[2].status === 'fulfilled' && it[2].value === 3
+    ) asyncTestPassed();
+  });
+
+};
