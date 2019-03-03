350779863919904b122ed810649e2fb1f6ad9bb6
Travis CI <travis@travis-ci.org>
2019-03-03 03:55:16 +0000

    update nightly result with 20190303.0.0-nightly (travis: 134) [skip ci]

--- esnext/1.0-SNAPSHOT/candidate__stage_3_/BigInt/DataView.prototype.getBigInt64/in.js
+++ esnext/1.0-SNAPSHOT/candidate__stage_3_/BigInt/DataView.prototype.getBigInt64/in.js
@@ -1,5 +1,8 @@
 // candidate (stage 3) / BigInt / DataView.prototype.getBigInt64
 module.exports = () => {
-  return typeof DataView.prototype.getBigInt64 === 'function';
+var buffer = new ArrayBuffer(64);
+var view = new DataView(buffer);
+view.setBigInt64(0, 1n);
+return view.getBigInt64(0) === 1n;
 
 };
