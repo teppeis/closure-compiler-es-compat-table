e2dd546e5d74a3aa197f3fd4ef4d85060956a6b0
teppeis <teppeis@gmail.com>
2019-04-02 01:00:48 +0900

    copy v20190301 to latest

--- esnext/latest/candidate__stage_3_/numeric_separators/in.js
+++ esnext/latest/candidate__stage_3_/numeric_separators/in.js
@@ -1,4 +1,4 @@
-// draft (stage 2) / numeric separators
+// candidate (stage 3) / numeric separators
 module.exports = () => {
 return 1_000_000.000_001 === 1000000.000001 &&
 0b1010_0001_1000_0101 === 0b1010000110000101;
--- esnext/latest/proposal__stage_1_/String.prototype.codePoints/out.js
+++ esnext/latest/proposal__stage_1_/String.prototype.codePoints/out.js
@@ -17,6 +17,6 @@ module.exports = function() {
   for (var a = [], b = $jscomp.makeIterator("a\ud842\udfb7b".codePoints()), c = b.next(); !c.done; c = b.next()) {
     a.push(c.value);
   }
-  return 3 === a.length && 97 === a[0] && 134071 === a[1] && 98 === a[2];
+  return 3 === a.length && 97 === a[0].codePoint && 0 === a[0].position && 134071 === a[1].codePoint && 1 === a[1].position && 98 === a[2].codePoint && 3 === a[2].position;
 };
 
