cb120c8fdf4b5923d00e9ad932dd57e198233d6c
Travis CI <travis@travis-ci.org>
2019-06-30 04:52:40 +0000

    update nightly result with 20190630.0.0-nightly (travis: 342) [skip ci]

--- es2016plus/nightly/2019_features/string_trimming/String.prototype.trimStart/out.js
+++ es2016plus/nightly/2019_features/string_trimming/String.prototype.trimStart/out.js
@@ -4,28 +4,34 @@ $jscomp.ASSUME_ES5 = !1;
 $jscomp.ASSUME_NO_NATIVE_MAP = !1;
 $jscomp.ASSUME_NO_NATIVE_SET = !1;
 $jscomp.SIMPLE_FROUND_POLYFILL = !1;
-$jscomp.defineProperty = $jscomp.ASSUME_ES5 || "function" == typeof Object.defineProperties ? Object.defineProperty : function(a, c, b) {
-  a != Array.prototype && a != Object.prototype && (a[c] = b.value);
+$jscomp.defineProperty = $jscomp.ASSUME_ES5 || "function" == typeof Object.defineProperties ? Object.defineProperty : function(a, b, c) {
+  a != Array.prototype && a != Object.prototype && (a[b] = c.value);
 };
 $jscomp.getGlobal = function(a) {
   return "undefined" != typeof window && window === a ? a : "undefined" != typeof global && null != global ? global : a;
 };
 $jscomp.global = $jscomp.getGlobal(this);
-$jscomp.polyfill = function(a, c, b, d) {
-  if (c) {
-    b = $jscomp.global;
+$jscomp.polyfill = function(a, b, c, d) {
+  if (b) {
+    c = $jscomp.global;
     a = a.split(".");
     for (d = 0; d < a.length - 1; d++) {
       var e = a[d];
-      e in b || (b[e] = {});
-      b = b[e];
+      e in c || (c[e] = {});
+      c = c[e];
     }
     a = a[a.length - 1];
-    d = b[a];
-    c = c(d);
-    c != d && null != c && $jscomp.defineProperty(b, a, {configurable:!0, writable:!0, value:c});
+    d = c[a];
+    b = b(d);
+    b != d && null != b && $jscomp.defineProperty(c, a, {configurable:!0, writable:!0, value:b});
   }
 };
+$jscomp.polyfill("String.prototype.trimLeft", function(a) {
+  function b() {
+    return this.replace(/^[\s\xa0]+/, "");
+  }
+  return a || b;
+}, "es_2019", "es3");
 $jscomp.polyfill("String.prototype.trimStart", function(a) {
   return a || String.prototype.trimLeft;
 }, "es_2019", "es3");
--- esnext/nightly/draft__stage_2_/weak_references/in.js
+++
@@ -1,9 +0,0 @@
-// draft (stage 2) / weak references
-module.exports = () => {
-  var O = {};
-  var weakref = System.makeWeakRef(O);
-  var works = weakref.get() === O;
-  weakref.clear();
-  return works && weakref.get() === undefined;
-
-};
