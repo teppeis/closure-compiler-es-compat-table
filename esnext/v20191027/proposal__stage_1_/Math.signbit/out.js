f08b4b93605b080f57d11c417fe19ea94074676c
teppeis <teppeis@gmail.com>
2019-11-15 12:55:39 +0900

    update result v20191027

--- esnext/v20191027/proposal__stage_1_/Math.signbit/out.js
+++ esnext/v20191027/proposal__stage_1_/Math.signbit/out.js
@@ -1,4 +1,4 @@
 module.exports = function() {
-  return !1 === Math.signbit(-0) && !0 === Math.signbit(0) && !1 === Math.signbit(-42) && !0 === Math.signbit(42);
+  return !1 === Math.signbit(NaN) && !0 === Math.signbit(-0) && !1 === Math.signbit(0) && !0 === Math.signbit(-42) && !1 === Math.signbit(42);
 };
 
