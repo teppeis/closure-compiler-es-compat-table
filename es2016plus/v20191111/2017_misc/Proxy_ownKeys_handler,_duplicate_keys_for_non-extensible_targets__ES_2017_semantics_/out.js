9a7a3887098464b042ef7e6e2f2262907a2a6ff6
teppeis <teppeis@gmail.com>
2019-12-02 14:21:49 +0900

    update result v20191111

--- es2016plus/v20191111/2017_misc/Proxy_ownKeys_handler,_duplicate_keys_for_non-extensible_targets__ES_2017_semantics_/out.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-  var a = new Proxy(Object.preventExtensions(Object.defineProperty({a:1}, "b", {value:1})), {ownKeys:function() {
-    return ["a", "a", "b", "b"];
-  }});
-  return "a,a,b,b" === Object.getOwnPropertyNames(a) + "";
-};
-
