bf00fd6c71df0d6f9ec5d057ce84c871c90fd1aa
teppeis <teppeis@gmail.com>
2020-01-19 22:01:07 +0900

    fix: lint error

--- batch/google-closure-compiler-java-dummy.js
+++ batch/google-closure-compiler-java-dummy.js
@@ -1,3 +1,8 @@
-// Webpack replaces require("google-closure-compiler-java") with this file.
-// The exported value is not used, instead native images are used.
+/**
+ * @fileoverview Webpack replaces require("google-closure-compiler-java") with this file.
+ * The exported value is not used, instead native images are used.
+ */
+
+"use strict";
+
 module.exports = "";
