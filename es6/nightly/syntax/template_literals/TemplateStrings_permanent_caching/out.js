6eb497258ca91e20635472bcbc3ec591bc6ed916
Travis CI <travis@travis-ci.org>
2020-03-06 07:03:20 +0000

    update nightly result with 20200306.0.0-nightly (travis: 835) [skip ci]

--- es6/nightly/syntax/template_literals/TemplateStrings_permanent_caching/out.js
+++ es6/nightly/syntax/template_literals/TemplateStrings_permanent_caching/out.js
@@ -1,5 +1,12 @@
-var $jscomp$templatelit$0 = ["foo"];
-$jscomp$templatelit$0.raw = $jscomp$templatelit$0.slice();
+var $jscomp$templatelit$0 = $jscomp.createTemplateTagFirstArg(["foo"]), $jscomp = $jscomp || {};
+$jscomp.scope = {};
+$jscomp.createTemplateTagFirstArg = function(a) {
+  return a.raw = a;
+};
+$jscomp.createTemplateTagFirstArgWithRaw = function(a, b) {
+  a.raw = b;
+  return a;
+};
 module.exports = function() {
   var a = $jscomp$templatelit$0, b = new function() {
     return $jscomp$templatelit$0;
--- esnext/nightly/draft__stage_2_/Array.isTemplateObject/out.js
+++ esnext/nightly/draft__stage_2_/Array.isTemplateObject/out.js
@@ -1,5 +1,12 @@
-var $jscomp$templatelit$0 = ["a{1}c"];
-$jscomp$templatelit$0.raw = $jscomp$templatelit$0.slice();
+var $jscomp$templatelit$0 = $jscomp.createTemplateTagFirstArg(["a{1}c"]), $jscomp = $jscomp || {};
+$jscomp.scope = {};
+$jscomp.createTemplateTagFirstArg = function(a) {
+  return a.raw = a;
+};
+$jscomp.createTemplateTagFirstArgWithRaw = function(a, b) {
+  a.raw = b;
+  return a;
+};
 module.exports = function() {
   return !Array.isTemplateObject([]) && Array.isTemplateObject($jscomp$templatelit$0);
 };
