7cb508fb2783f8e23c8b2771d77924b7d5673663
teppeis <teppeis@gmail.com>
2020-05-19 23:56:06 +0900

    update compat-table

--- alter-tests/es6/syntax/Unicode_code_point_escapes/in_identifiers/orig.js
+++ alter-tests/es6/syntax/Unicode_code_point_escapes/in_identifiers/orig.js
@@ -1,6 +1,6 @@
 // syntax / Unicode code point escapes / in identifiers
 module.exports = () => {
-  var \u{102C0} = { \u{102C0} : 2 };
-  return \u{102C0}['\ud800\udec0'] === 2;
+  var \u{102C0} = 2;
+  return \u{102C0} === 2;
 
 };
