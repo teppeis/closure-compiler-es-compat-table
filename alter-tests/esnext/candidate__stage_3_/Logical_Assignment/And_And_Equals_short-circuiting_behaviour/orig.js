0ca4193d0b20c56bf6b91c5ed1232ac563dd82e4
teppeis <teppeis@gmail.com>
2020-08-22 21:05:51 +0900

    udpate compat-table

--- alter-tests/esnext/candidate__stage_3_/Logical_Assignment/And_And_Equals_short-circuiting_behaviour/orig.js
+++
@@ -1,8 +0,0 @@
-// candidate (stage 3) / Logical Assignment / &&= short-circuiting behaviour
-module.exports = () => {
-let a;
-let i = 1;
-a &&= ++i;
-return typeof a === 'undefined' && i === 1;
-
-};
