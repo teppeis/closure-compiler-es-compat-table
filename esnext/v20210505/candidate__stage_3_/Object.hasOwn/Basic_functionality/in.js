166d21bddb9d9538c7a0d6d4fbce82fe4b497e44
teppeis <teppeis@gmail.com>
2021-06-13 17:40:46 +0900

    update result

---
+++ esnext/v20210505/candidate__stage_3_/Object.hasOwn/Basic_functionality/in.js
@@ -0,0 +1,5 @@
+// candidate (stage 3) / Object.hasOwn / Basic functionality
+module.exports = () => {
+  return Object.hasOwn({ x: 2 }, "x") === true;
+
+};
--- esnext/v20210505/candidate__stage_3_/instance_class_fields/computed_instance_class_fields/in.js
+++
@@ -1,8 +0,0 @@
-// candidate (stage 3) / instance class fields / computed instance class fields
-module.exports = () => {
-class C {
-['x'] = 42;
-}
-return new C().x === 42;
-
-};
