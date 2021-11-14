0cc90e7d4a81d9cdba857cd7765393c144d88f46
teppeis <teppeis@gmail.com>
2021-11-14 21:41:37 +0900

    update compat-table

--- alter-tests/esnext/Stage_3/Error.cause_property/ReferenceError_has_cause/orig.js
+++
@@ -1,6 +0,0 @@
-// Stage 3 / Error.cause property / ReferenceError has cause
-module.exports = () => {
-  var error = new ReferenceError('error', { cause: 'cause' })
-  return error.hasOwnProperty('cause') && error.cause === 'cause';
-
-};
--- alter-tests/esnext/Stage_3/Error.cause_property/TypeError_has_cause/orig.js
+++
@@ -1,6 +0,0 @@
-// Stage 3 / Error.cause property / TypeError has cause
-module.exports = () => {
-  var error = new TypeError('error', { cause: 'cause' })
-  return error.hasOwnProperty('cause') && error.cause === 'cause';
-
-};
