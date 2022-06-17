f175bde6636436f57776fd54f933285dbbde23de
teppeis <teppeis@gmail.com>
2022-06-17 23:13:33 +0900

    copy v20220405 to latest

--- es6/latest/syntax/template_literals/passed_array_is_frozen/out.js
+++ es6/latest/syntax/template_literals/passed_array_is_frozen/out.js
@@ -7,9 +7,9 @@ $jscomp.createTemplateTagFirstArgWithRaw = function(a, b) {
   a.raw = b;
   return a;
 };
-var $jscomp$templatelit$m2031426729$0 = $jscomp.createTemplateTagFirstArg(["foo", "bar", "baz"]);
+var $jscomp$templatelit$1372602716$0 = $jscomp.createTemplateTagFirstArg(["foo", "bar", "baz"]);
 module.exports = function() {
-  var a = $jscomp$templatelit$m2031426729$0;
+  var a = $jscomp$templatelit$1372602716$0;
   return Object.isFrozen(a) && Object.isFrozen(a.raw);
 };
 
--- esnext/latest/Stage_2/Array.isTemplateObject/out.js
+++ esnext/latest/Stage_2/Array.isTemplateObject/out.js
@@ -7,8 +7,8 @@ $jscomp.createTemplateTagFirstArgWithRaw = function(a, b) {
   a.raw = b;
   return a;
 };
-var $jscomp$templatelit$m1041919362$0 = $jscomp.createTemplateTagFirstArg(["a{1}c"]);
+var $jscomp$templatelit$m1639057959$0 = $jscomp.createTemplateTagFirstArg(["a{1}c"]);
 module.exports = function() {
-  return !Array.isTemplateObject([]) && Array.isTemplateObject($jscomp$templatelit$m1041919362$0);
+  return !Array.isTemplateObject([]) && Array.isTemplateObject($jscomp$templatelit$m1639057959$0);
 };
 
