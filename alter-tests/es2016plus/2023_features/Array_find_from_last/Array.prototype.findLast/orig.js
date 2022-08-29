d9cfe5fb28f1b5d24dc1bd1428309bb1f8f4b843
teppeis <teppeis@gmail.com>
2022-08-29 11:21:24 +0900

    update compat-table

--- alter-tests/es2016plus/2023_features/Array_find_from_last/Array.prototype.findLast/orig.js
+++ alter-tests/es2016plus/2023_features/Array_find_from_last/Array.prototype.findLast/orig.js
@@ -1,4 +1,4 @@
-// Stage 3 / Array find from last / Array.prototype.findLast
+// 2023 features / Array find from last / Array.prototype.findLast
 module.exports = () => {
   var arr = [{ x: 1 }, { x: 2 }, { x: 1 }, { x: 2 }];
   return arr.findLast(function (o) { return o.x === 1; }) === arr[2];
