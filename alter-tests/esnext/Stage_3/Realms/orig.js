b9bfead1802d8fcfb0d2413979b31d7979f20a2d
teppeis <teppeis@gmail.com>
2023-05-25 22:50:37 +0900

    update compat-table

--- alter-tests/esnext/Stage_3/Realms/orig.js
+++
@@ -1,8 +0,0 @@
-// Stage 3 / Realms
-module.exports = () => {
-  return typeof Realm === "function"
-&& ["eval", "global", "intrinsics", "stdlib", "directEval", "indirectEval", "initGlobal", "nonEval"].every(function(key){
-  return key in Realm.prototype;
-});
-
-};
