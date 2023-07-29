450b9d0419f515a0c596dd56c05dafb940a81430
teppeis <teppeis@gmail.com>
2023-07-29 21:33:13 +0900

    update compat-table

--- alter-tests/es6/syntax/destructuring,_parameters/duplicate_identifier/orig.js
+++ alter-tests/es6/syntax/destructuring,_parameters/duplicate_identifier/orig.js
@@ -1,8 +1,11 @@
 // syntax / destructuring, parameters / duplicate identifier
 module.exports = () => {
   throw new Error('eval() and Function() cannot be transpiled');
-  var d = function d([d]) { return d };
-  if (d([true]) !== true) return false;
+  try {
+    eval('var d = function d([d]) { return d };if (d([true]) !== true) return false;');
+  } catch (e) {
+    return !(e instanceof SyntaxError);
+  }
   try {
     eval('var f = function f([id, id]) { return id }');
     return false;
